c530b5dffd72d7502998ffedfb96d1dcec4201f6
Svetla Alexandrova <121426643+Svetla-Alexandrova@users.noreply.github.com>
2022-12-30 10:52:22 +0000

    Send emails to contacts in a list with attachment

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import yagmail
 import os
-import time
+import pandas
+
 
 sender = 'mmmm@gmail.com'
-receiver = 'nnn@gmail.com'
 
 subject = 'This is the subject'
-content = """
-Here is the content of the email
-"""
-
 
 yag = yagmail.SMTP(user=sender, password=os.getenv("PASSWORD"))
 # First select Tools/Secrets; Give name to the key, then go to your gmail and apply apps password
 # Uses getenv to prevent the vialization of password from other users.
 
-while True:
+df = pandas.read_csv('contacts.csv')
+
+for index, row in df.iterrows():
+  receiver = row['email']
+  content = [f"""Hi, {row['name']},
+You have to pay {row['amount']}""", 'text.txt']
   yag.send(to=receiver, subject=subject, contents=content)
   print('Email is sent')
-  time.sleep(60)
