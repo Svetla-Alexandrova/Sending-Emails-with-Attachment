5a466967f52dd05a2374dace9cfd872087c39d4d
Svetla Alexandrova <121426643+Svetla-Alexandrova@users.noreply.github.com>
2022-12-30 10:09:29 +0000

    Sending emails every minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import yagmail
 import os
+import time
 
 sender = 'mmmm@gmail.com'
 receiver = 'nnn@gmail.com'
@@ -13,4 +14,8 @@ Here is the content of the email
 yag = yagmail.SMTP(user=sender, password=os.getenv("PASSWORD"))
 # First select Tools/Secrets; Give name to the key, then go to your gmail and apply apps password
 # Uses getenv to prevent the vialization of password from other users.
-yag.send(to=receiver, subject=subject, contents=content)
+
+while True:
+  yag.send(to=receiver, subject=subject, contents=content)
+  print('Email is sent')
+  time.sleep(60)
